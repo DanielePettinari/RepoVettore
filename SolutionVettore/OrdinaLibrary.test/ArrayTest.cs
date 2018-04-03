aaa4662bc99f54d8f892d5e4eca5eaeaa5515708
Utente <Utente@UTENTE-PC>
2018-04-03 20:19:14 +0200

    Ho aggiunto alla soluzione il metodo e la soluzione

---
+++ SolutionVettore/OrdinaLibrary.test/ArrayTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OrdinaLibrary.test
+{
+    [TestClass]
+    public class ArrayTest
+    {
+        [TestMethod]
+        public void TestArrayOrdinamento()
+        {
+            int[] v1 = new int[] { 11, 22, 13, 4 };
+            int[] v2 = new int[] { 4, 11, 3, 22 };
+            int[] vettoreOrdinato = Vettore.Ordina(v1);
+            CollectionAssert.AreEqual(v2, vettoreOrdinato);
+        }
+    }
+}
---
+++ SolutionVettore/OrdinaLibrary/Array.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrdinaLibrary
+{
+    public class Array
+    {
+        public class Vettore
+        {
+            public static int[] Ordina(int[] numeri)
+            {
+                for (int i = 0; i < numeri.Length - 1; i++)
+                {
+                    for (int j = 0; j < numeri.Length; j++)
+                    {
+                        if (numeri[i] > numeri[j])
+                        {
+                            int tmp = numeri[i];
+                            numeri[i] = numeri[j];
+                            numeri[j] = tmp;
+                        }
+                    }
+                }
+                return numeri;
+            }
+        }
+    }
+}
